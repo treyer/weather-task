a2e554b0c7c2c0c58fd840ddbda852dbd1079bee
treyer <nemereno_by@mail.ru>
2022-11-28 06:05:57 +0300

    fix: change ipwho url to secure

--- weather-app/src/constants/urls.ts
+++ weather-app/src/constants/urls.ts
@@ -1,5 +1,5 @@
 export const UNSPLASH_URL = "https://api.unsplash.com/photos/random?";
-export const IP_WHO_URL = "http://ipwho.is/";
+export const IP_WHO_URL = "https://ipwho.is/";
 export const IP_GEOLOCATON_URL = "https://api.ipgeolocation.io/timezone?";
 export const OPENWEATHERMAP_URL =
   "https://api.openweathermap.org/data/2.5/forecast?";
